3a0cd964fec534e25567428b6c8f6cd409e660f1
coderganesh <ganeshkumartk@outlook.com>
2019-12-21 12:08:25 +0530

    Updates

---
+++ precache-manifest.1a8504b7596d1dbb9cef9894341c730f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0642a33b5a5c2f3c7fc2ecddc1cf5d9e",
+    "url": "/pic2pill/index.html"
+  },
+  {
+    "revision": "d22c309c8073aa6814f3",
+    "url": "/pic2pill/static/css/2.b138a779.chunk.css"
+  },
+  {
+    "revision": "723bac0f38aec17758fe",
+    "url": "/pic2pill/static/css/main.ce323a4c.chunk.css"
+  },
+  {
+    "revision": "d22c309c8073aa6814f3",
+    "url": "/pic2pill/static/js/2.faec1bcf.chunk.js"
+  },
+  {
+    "revision": "336596b126345f0765956241be760aa8",
+    "url": "/pic2pill/static/js/2.faec1bcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "723bac0f38aec17758fe",
+    "url": "/pic2pill/static/js/main.a5a0e501.chunk.js"
+  },
+  {
+    "revision": "173082247e46a2da30d3",
+    "url": "/pic2pill/static/js/runtime-main.660e9606.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pic2pill/precache-manifest.4490000bee5db39abfdedf952cd686ed.js"
+  "/pic2pill/precache-manifest.1a8504b7596d1dbb9cef9894341c730f.js"
 );
 
 self.addEventListener('message', (event) => {
